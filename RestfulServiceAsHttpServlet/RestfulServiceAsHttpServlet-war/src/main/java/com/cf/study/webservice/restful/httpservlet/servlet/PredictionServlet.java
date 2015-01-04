eca548bde142a41a09050d5b021cf1284c4d3029
Fei Chen <sdcf2010@gmail.com>
2015-01-04 16:04:57 +0000

    Finalize project

--- RestfulServiceAsHttpServlet/RestfulServiceAsHttpServlet-war/src/main/java/com/cf/study/webservice/restful/httpservlet/servlet/PredictionServlet.java
+++ RestfulServiceAsHttpServlet/RestfulServiceAsHttpServlet-war/src/main/java/com/cf/study/webservice/restful/httpservlet/servlet/PredictionServlet.java
@@ -11,12 +11,17 @@
  *----------------------------------------------------------------------------*/
 package com.cf.study.webservice.restful.httpservlet.servlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.concurrent.ConcurrentMap;
 
-import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.xml.ws.http.HTTPException;
 
 import org.json.JSONObject;
@@ -26,6 +31,7 @@ import com.cf.study.webservice.restful.httpservlet.Prediction;
 import com.cf.study.webservice.restful.httpservlet.PredictionRepository;
 import com.cf.study.webservice.restful.httpservlet.util.SerializationUtility;
 
+@WebServlet("/Prediction")
 public class PredictionServlet extends HttpServlet {
 
     private static final long serialVersionUID = -3568946092246651984L;
@@ -44,13 +50,23 @@ public class PredictionServlet extends HttpServlet {
 
     @Override
     public void doPost(final HttpServletRequest request, final HttpServletResponse response) {
-
+        final String who = request.getParameter(WHO_PARAM);
+        final String what = request.getParameter(WHAT_PARAM);
+        if (who == null || what == null) {
+            throw new HTTPException(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        final Prediction p = new Prediction();
+        p.setWho(who);
+        p.setWhat(what);
+        final int id = predictionRepository.addPrediction(p);
+        final String message = "Prediction " + id + " created.\n";
+        sendResponse(response, SerializationUtility.toXML(message), false);
     }
 
     @Override
     public void doGet(final HttpServletRequest request, final HttpServletResponse response) {
         final String id = request.getParameter(ID_PARAM);
-        final Integer key = (id == null) ? null : new Integer(id.trim());
+        final Integer key = id == null ? null : new Integer(id.trim());
         final boolean json = acceptJson(request);
         if (key == null) {
             final ConcurrentMap<Integer, Prediction> map = predictionRepository.getPredictions();
@@ -62,7 +78,7 @@ public class PredictionServlet extends HttpServlet {
             final Prediction prediction = predictionRepository.getPredictions().get(key);
             if (prediction == null) {
                 final String msg = "Prediction with id=" + key + " does not exist!\n";
-                sendResponse(response, msg, json);
+                sendResponse(response, SerializationUtility.toXML(msg), false);
             } else {
                 sendResponse(response, SerializationUtility.toXML(prediction), json);
             }
@@ -71,17 +87,80 @@ public class PredictionServlet extends HttpServlet {
 
     @Override
     public void doPut(final HttpServletRequest request, final HttpServletResponse response) {
-
+        String key = null;
+        String rest = null;
+        boolean who = false;
+        try {
+            final BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+            final String data = br.readLine();
+            // The data should looks like: id=33#who=Homer Allision
+            final String[] args = data.split("#");
+            final String[] parts1 = args[0].split("=");
+            key = parts1[1];
+            final String[] parts2 = args[1].split("=");
+            if (parts2[0].contains("who")) {
+                who = true;
+            }
+            rest = parts2[1];
+        } catch (final Exception e) {
+            throw new HTTPException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        if (key == null) {
+            throw new HTTPException(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        final Prediction p = predictionRepository.getPredictions().get(new Integer(key.trim()));
+        if (p == null) {
+            final String msg = key + " does not map to a Prediction.\n";
+            sendResponse(response, SerializationUtility.toXML(msg), false);
+        } else {
+            if (rest == null) {
+                throw new HTTPException(HttpServletResponse.SC_BAD_REQUEST);
+            } else {
+                if (who) {
+                    p.setWho(rest);
+                } else {
+                    p.setWhat(rest);
+                }
+                final String msg = "Prediction " + key + " has been edited.\n";
+                sendResponse(response, SerializationUtility.toXML(msg), false);
+            }
+        }
     }
 
     @Override
     public void doDelete(final HttpServletRequest request, final HttpServletResponse response) {
+        final String id = request.getParameter(ID_PARAM);
+        final Integer key = id == null ? null : new Integer(id.trim());
+        if (key == null) {
+            throw new HTTPException(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        try {
+            predictionRepository.getPredictions().remove(key);
+            final String msg = "Prediction " + key + " removed.\n";
+            sendResponse(response, SerializationUtility.toXML(msg), false);
+        } catch (final Exception e) {
+            throw new HTTPException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
 
+    @Override
+    public void doTrace(final HttpServletRequest request, final HttpServletResponse response) {
+        throw new HTTPException(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+    }
+
+    @Override
+    public void doHead(final HttpServletRequest request, final HttpServletResponse response) {
+        throw new HTTPException(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+    }
+
+    @Override
+    public void doOptions(final HttpServletRequest request, final HttpServletResponse response) {
+        throw new HTTPException(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
     }
 
     private boolean acceptJson(final HttpServletRequest request) {
         boolean json = false;
-        final String accept = request.getParameter(FORMAT_ACCEPT_PARAM);
+        final String accept = request.getHeader(FORMAT_ACCEPT_PARAM);
         if (accept != null && accept.contains("json")) {
             json = true;
         }
@@ -91,7 +170,8 @@ public class PredictionServlet extends HttpServlet {
     private void sendResponse(final HttpServletResponse response, String payload, final boolean json) {
         if (json) {
             final JSONObject jsonObject = XML.toJSONObject(payload);
-            payload = jsonObject.toString(3); // 3 is indentation level for nice look
+            payload = jsonObject.toString(3);
+            // 3 is indentation level for nice look
         }
         try {
             final OutputStream out = response.getOutputStream();
